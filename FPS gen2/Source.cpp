d8783779136e313871951fba228f74eabcce00dc
WitoldSierka <115167841+WitoldSierka@users.noreply.github.com>
2023-07-27 20:57:44 +0200

    Fixed visual glitches
    Minimap can be toggled on / off, press the 'M' key. 
    Walls' color has been changed to green, as it is easier on the eyes.

--- FPS gen2/Source.cpp
+++ FPS gen2/Source.cpp
@@ -23,6 +23,7 @@ int sourceMapWidth = 8;
 int corridorWidth = 3;
 int mapHeight = sourceMapHeight * (corridorWidth + 1) + 1;
 int mapWidth = sourceMapWidth * (corridorWidth + 1) + 1;
+int mapColor = 2;
 float FOV = PI / 3;
 
 class Player {
@@ -36,7 +37,8 @@ enum PIXEL_RANGE {
 	PX100 = 0x2588,
 	PX75 = 0x2593,
 	PX50 = 0x2592,
-	PX25 = 0x2591
+	PX25 = 0x2591,
+	PX00 = 160  //non-breaking space
 };
 
 void clearScreen(std::vector<CHAR_INFO>& s);
@@ -65,6 +67,7 @@ std::vector<int> test_map{ {
 		1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
 	} };
 // - - - - COLORS OF THE WINDOW CONSOLE - - - -
+// 0 - black
 // 1 - blue
 // 2 - green
 // 3 - light blue
@@ -97,9 +100,11 @@ int main() {
 	int result = createConsole(windowSize, console);
 	if (result) { return result; }
 	bool gameOver = false;
+	bool mapToggle = true;
 	auto tp1 = std::chrono::system_clock::now();
 	auto tp2 = std::chrono::system_clock::now();
 	Player p;
+	float ROV = 16.0f; //range of view
 
 	while (!gameOver) {
 		clearScreen(screen);
@@ -136,8 +141,10 @@ int main() {
 				p.Y += sinf(p.A) * elapsedTime * 5.0f;
 			}
 		}
+		if (GetAsyncKeyState((unsigned short)'M') & 0x8000) {
+			mapToggle = mapToggle ? false : true;
+		}
 		// - - - - - RAYCASTING - - - - -
-		float ROV = 16.0f; //range of view
 
 		float dx = ceilf(p.X) - p.X; // X offset within tile
 		float d2x = p.X - floorf(p.X);
@@ -177,7 +184,6 @@ int main() {
 				int mapY = lookUp ? (int)hY : (int)hY - 1;
 				//test if ray is out of bounds
 				if (mapX < 0 || mapX >= mapWidth || mapY < 0 || mapY >= mapHeight) {
-					disH = ROV;
 					break;
 				} else {
 					mapPosH = mapY * mapWidth + mapX;
@@ -214,7 +220,6 @@ int main() {
 				int mapY = (int)vY;
 				//test if ray is out of bounds
 				if (mapX < 0 || mapX >= mapWidth || mapY < 0 || mapY >= mapHeight) {
-					disV = ROV;
 					break;
 				} else {
 					mapPosV = mapY * mapWidth + mapX;
@@ -227,54 +232,56 @@ int main() {
 					}
 				}
 			}
-			int horizontalShade = 0;
+			//int horizontalShade = 0;
 			if (disH <= disV) {
 				distanceToWall = disH;
 				mapPos = mapPosH;
-				horizontalShade = 0x0000; //"" - microsoft
-				if (abs(hX - trunc(hX)) < 0.03) {hitCorner = true;}
+				//horizontalShade = 0x80;
+				if (abs(hX - trunc(hX)) < 0.015 || abs(ceil(hX) - hX) < 0.015) {hitCorner = true;}
 			} else {
 				distanceToWall = disV;
 				mapPos = mapPosV;
 				if (abs(vY - trunc(vY)) < 0.03) {hitCorner = true;}
 			}
-			//distanceToWall = disH;
-			// Calculate distance to ceiling and floor
-			int ceiling = float(screenHeight / 2.0 - screenHeight / ((float)distanceToWall));
+			
+			// Calculate ceiling and floor thresholds
+			int ceiling = static_cast<int>(screenHeight / 2.0f - screenHeight / (float)distanceToWall);
 			int floor = screenHeight - ceiling;
 
 			//Shading using Unicode Characters
 			int shade = ' ';
 			if (hitCorner) { shade = ' '; }
-			else if (distanceToWall <= ROV * 0.25) { shade = 0x2588; } // very close
-			else if (distanceToWall < ROV * 0.5) { shade = 0x2593; }
-			else if (distanceToWall < ROV * 0.75) { shade = 0x2592; }
-			else if (distanceToWall < ROV) { shade = 0x2591; }
-			else { shade = ' '; }    //too far away
+			else if (distanceToWall < ROV * 0.25)	{ shade = PX100;}
+			else if (distanceToWall < ROV * 0.5)	{ shade = PX75; }
+			else if (distanceToWall < ROV * 0.75)	{ shade = PX50; }
+			else if (distanceToWall < ROV)			{ shade = PX25; }
+			else									{ shade = PX00; }
 
 			for (int y = 0; y < screenHeight; y++) {
+				screen[y * screenWidth + x].Attributes = map[mapPos] ? map[mapPos] : mapColor;
 				if (y <= ceiling) {
-					screen[y * screenWidth + x].Char.AsciiChar = 96;
+					screen[y * screenWidth + x].Char.UnicodeChar = '`';
 				} else if (y > ceiling && y <= floor) {
-					screen[y * screenWidth + x].Char.AsciiChar = shade;
+					screen[y * screenWidth + x].Char.UnicodeChar = shade;
+					//screen[y * screenWidth + x].Attributes |= horizontalShade;
 				} else {
 					//Shade floor based on distance
 					float floorDist = 1.0f - (((float)y - screenHeight / 2.0f) / ((float)screenHeight / 2.0f));
 					int floorShade = ' ';
-					if (floorDist < 0.25) { floorShade = '#'; }
-					else if (floorDist < 0.6) { floorShade = '-'; }//-
-					else if (floorDist < 0.8) { floorShade = '.'; }//.
-					else/* if (floorDist < 0.9)*/ { floorShade = 'x'; }//x
-					//else						{ shade = 32; }
-					screen[y * screenWidth + x].Char.AsciiChar = floorShade;
+					if (floorDist < 0.25)	  { floorShade = 'x'; }
+					else if (floorDist < 0.6) { floorShade = '+'; }
+					else if (floorDist < 0.8) { floorShade = '-'; }
+					else					  { floorShade = '.'; }
+					screen[y * screenWidth + x].Char.UnicodeChar = floorShade;
 				}
-			screen[y * screenWidth + x].Attributes = map[mapPos] | horizontalShade;
 			}
 			mapPos = 0;
 		}
 		// - - - - - RENDERING - - - - -
-		draw_map(screen, map, mapWidth, mapHeight, screenWidth, p, 3);
-		float fps = 1.0f / elapsedTime; //get framerate
+		if (mapToggle) {
+			draw_map(screen, map, mapWidth, mapHeight, screenWidth, p, 3);
+		}
+		float fps = 1.0f / elapsedTime;
 		std::wstring title = std::to_wstring(fps);
 		SetConsoleTitle(title.data());
 		WriteConsoleOutput(console, screen.data(), {short(screenWidth), short(screenHeight)}, {0, 0}, &windowSize);
@@ -317,19 +324,18 @@ int createConsole(SMALL_RECT &w, HANDLE &h) {
 		// "window error"
 		return 7;
 	}
-	//if (!SetConsoleMode(consoleIn, ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT))  < - - - mouse input
 	return 0;
 }
 
-//screen, map to draw, map width, map height, screen width, pixel scale
+//screen, map to draw, map width, map height, screen width, player position, pixel scale
 void draw_map(std::vector<CHAR_INFO> &s, const std::vector<int> &m, size_t mw, size_t mh, size_t sw, const Player &p, size_t ps) {
-	for (size_t i = 0; i < mh; i++) {
-		for (size_t j = 0; j < mw; j++) {
-			int pixelChar = 96;
+	for (int i = 0; i < mh; i++) {
+		for (int j = 0; j < mw; j++) {
+			int pixelChar = PX00;
 			if (m[i * mh + j]) { pixelChar = PX100; }
 			for (size_t k = 0; k < ps; k++) {
 				for (size_t l = 0; l < ps; l++) {
-					s[(i * ps + k) * sw + j*ps + l].Char.AsciiChar = pixelChar;
+					s[(i * ps + k) * sw + j*ps + l].Char.UnicodeChar = pixelChar;
 				}
 			}
 		}
@@ -338,7 +344,7 @@ void draw_map(std::vector<CHAR_INFO> &s, const std::vector<int> &m, size_t mw, s
 		for (size_t j = 0; j < ps; j++) {
 			size_t pos = (int(p.Y) * ps + i) * sw + int(p.X) * ps + j;
 			s[pos].Attributes = 4;
-			s[pos].Char.AsciiChar = PX100;
+			s[pos].Char.UnicodeChar = PX100;
 		}
 	}
 }
@@ -376,7 +382,7 @@ std::vector<int> randomize_map(int width, int height, int cw) {
 	for (int i = 0; i < rh; ++i) {
 		for (int j = 0; j < rw; j++) {
 			if (i%cw == 0 || j%cw == 0) {
-				res_map[i * rw + j] = 3;
+				res_map[i * rw + j] = mapColor;
 			}
 		}
 	}
